c5810597d9b4aca00f3993ad801269840cd408d4
arminhenres <henres.armin@live.de>
2015-02-25 18:02:12 +0100

    Changed GUI-Namespace

--- CharacterSheetManager/MainWindow.xaml.cs
+++ CharacterSheetManager/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CharacterSheetManager
+namespace CharacterSheetManager.GUI
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
